a1e5a35dbdb5b2d767983790cabe30f7dacae530
okuzawats <okuzawats@gmail.com>
2024-03-04 05:40:43 +0900

    IndexedDBへの保存処理を実装

--- src/db/memos.ts
+++ src/db/memos.ts
@@ -12,3 +12,9 @@ db.version(1).stores({ memos: '&datetime' }) // datetimeをindexに指定
 
 // データ型 - キーのペア。キーはdatetimeとする。
 const memos: Dexie.Table<MemoRecord, string> = db.table('memos')
+
+// 引数としてタイトルとテキストを受け取り、非同期で保存処理を実行する。
+export const putMemo = async(title: string, text: string): Promise<void> => {
+  const datetime = new Date().toISOString()
+  await memos.put({ datetime, title, text })
+}
