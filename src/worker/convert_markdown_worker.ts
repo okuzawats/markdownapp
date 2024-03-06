6f3e684a9b1e127b052699994a1a69fd60d697ab
okuzawats <okuzawats@gmail.com>
2024-03-06 09:08:46 +0900

    Web Workerを用いてmarkdownをhtmlに変換する処理を実装変換する処理を実装

--- src/worker/convert_markdown_worker.ts
+++ src/worker/convert_markdown_worker.ts
@@ -1,9 +1,12 @@
+import * as marked from 'marked'
+
 const worker: Worker = self as any // Web Worker
 
 // メインスレッドからデータを渡された時に実行する関数。
 // event.dataにメインスレッドから渡されたパラメータが格納されている。
 // Worker#postMessageでメインスレッドに処理結果を送信する。
 worker.addEventListener('message', (event) => {
-  console.log('Worker Received:', event.data)
-  worker.postMessage({ result: event.data })
+  const text = event.data
+  const html = marked(text)
+  worker.postMessage({ html })
 })
